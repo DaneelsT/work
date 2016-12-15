1aef46b051fc95cfb3f31997b07cf67ba1cc7015
Gaetan Dumortier <gaetan1995@gmail.com>
2016-12-15 01:16:07 +0100

    Add password encryption info-box on user registration page

--- theme/inc.user-verify.php
+++ theme/inc.user-verify.php
@@ -2,13 +2,13 @@
     <?php
     use \Carbon\Application\Application;
     $app = Application::getInstance();
-    
+
     if($this->invalidToken()) {
     ?>
     <div class="panel panelSeperatorTop">
         <div class="panelContent">
             <h2><?php echo translate("Invalid token"); ?>!</h2>
-            
+
             <p><?php echo translatevar("The token you have supplied (%s) does not appear to be valid", $app->getRouter()->getSegment(2)); ?></p>
         </div>
     </div>
@@ -16,7 +16,7 @@
     <div class="panel panelSeperatorTop">
         <div class="panelContent">
             <h2><?php echo translate("User already exists"); ?></h2>
-            
+
             <p><?php echo translate("A user with the supplied username or email already exists in the system"); ?>!</p>
         </div>
     </div>
@@ -24,7 +24,7 @@
     <div class="panel panelSeperatorTop">
         <div class="panelContent">
             <h2><?php echo translate("Account created"); ?>!</h2>
-            
+
             <p><?php echo translate("Your account has been created successfully"); ?>!</p>
             <a href="<?php placeHttpRoot(); ?>login/" class="button"><?php echo translate("Proceed to the login page"); ?></a>
         </div>
@@ -33,7 +33,7 @@
     <div class="panel panelSeparatorTop">
         <div class="panelContent">
             <h2><?php echo translate("Invalid Input"); ?></h2>
-            
+
             <p>
                 <?php echo translate("Some fields were left empty or were not filled in correctly"); ?>
             </p>
@@ -43,6 +43,9 @@
         </div>
     </div>
     <?php }elseif($this->isVerified()) { ?>
+    <div class="panel info-important">
+        <h4><?php echo translate("Your password will be encrypted upon registration. Nobody will be able to view or decrypt your password"); ?>.</h4>
+    </div>
     <div class="panel panelSeparatorTop">
         <div class="panelContent">
             <h2><?php echo translate("Finish your registration"); ?></h2>
@@ -69,7 +72,7 @@
                     <input type="radio" name="user_gender" value="1" checked><?php echo translate("Male"); ?>
                     <input type="radio" name="user_gender" class="spacingLeft" value="2"><?php echo translate("Female"); ?>
                 </div>
-                
+
                 <h4><?php echo translate("Payment Settings"); ?></h4>
                 <div class="formContainerSeperator formSeparator">
                     <label class="stdLabelWidth"><?php echo translate("Hourly Pay"); ?></label>
@@ -77,7 +80,7 @@
                     <label class="stdLabelWidth"><?php echo translate("Sunday Fee"); ?></label>
                     <input type="text" name="sunday_fee" placeholder="<?php echo $app->getConfiguration("sunday_fee"); ?>" size="1">
                 </div>
-                
+
                 <h4><?php echo translate("Language Settings"); ?></h4>
                 <div class="formContainerSeperator formSeparator">
                     <label class="stdLabelWidth"><?php echo translate("Language"); ?></label>
@@ -86,7 +89,7 @@
                         <option value="en_US">English</option>
                     </select>
                 </div>
-                
+
                 <h4><?php echo translate("User password"); ?></h4>
                 <div>
                     <label class="stdLabelWidth"><?php echo translate("Password"); ?></label>
