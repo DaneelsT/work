fbbd742761472cb35ab016c2c317c2ee5a655e70
Gaetan Dumortier <gaetan1995@gmail.com>
2017-05-10 06:28:46 +0200

    Remove conditional operator untill further implementation

--- theme/js/dashboard.js
+++ theme/js/dashboard.js
@@ -46,10 +46,6 @@ function checkSunday(date) {
     newDate.setMonth(dateArray[1]);
     newDate.setDate(dateArray[2]);
 
-    console.log(newDate.getFullYear());
-    console.log(newDate.getMonth());
-    console.log(newDate.getDate());
-
     if(newDate.getDay() == 0) {
         sundayStr = "SUNDAY";
     }else{
@@ -70,7 +66,7 @@ function addShift(id, date, startTime, endTime, isSunday) {
                 "<td>" + formatDate(startTime) + "</td>" +
                 "<td>" + formatDate(endTime) + "</td>" +
                 "<td>" + hoursWorked + "</td>" +
-                "<td style='color:#28AF28'>" + (isSunday == 1) ? checkSunday(date) : "" + "</td>" +
+                "<td style='color:#28AF28'>" + checkSunday(date) + "</td>" +
                 "<td>" +
                     "<a class='button right buttonRed' href='shift/remove/" + id + "'>Remove</a>" +
                     "<a class='button right buttonSpacingRight' href='shift/edit/" + id + "'>Edit</a></td>" +
