27c6639f156d1a81cdad314b17df78f2c5b088a5
Gaetan Dumortier <gaetan1995@gmail.com>
2017-05-10 05:02:15 +0200

    Implement shift-table appending of retrieved data

--- theme/js/dashboard.js
+++ theme/js/dashboard.js
@@ -8,36 +8,43 @@
 var base = "api/";
 var key = "dev"; // api key
 
-function addShift(date, startTime, endTime, isSunday) {
-    // TODO: implement.
+// Get the shifts from the user with provided userid using an HTTP GET
+function getShifts(userid) {
+    $.ajax({
+        url: base + "shifts/" + userid,
+        type: "GET",
+        dataType: "json",
+        headers: {
+            "Authorization": key,
+        },
+        success: function(data) {
+            for (var i = 0; i < data.length; i++) {
+                var result = data[i];
 
-    $("body")
-        .append(date)
-        .append(startTime);
+                addShift(result.date, result.startTime, result.endTime, result.isSunday);
+            }
+        },
+        error: function(error) {
+            console.log("error: " + error);
+        }
+    });
+}
+
+// Append a new shift to the shifts table with the provided arguments
+function addShift(date, startTime, endTime, isSunday) {
+    var sundayStr = "";
+    // idk if this will work lol
+    $("#shifts").append("<tr><td>" + date + "</td></tr>");
+    $("#shifts").append("<tr><td>" + startTime + "</td></tr>");
+    $("#shifts").append("<tr><td>" + endTime + "</td></tr>");
+    $("#shifts").append("<tr><td>" + isSunday + "</td></tr>");
 }
 
 $(document).ready(function() {
 
-    // try and fetch shifts from user with id 1 when testButton has been clicked.
+    // try and fetch shifts from user with id 1 when testButton has been clicked
     $("#testButton").click(function() {
-        $.ajax({
-            url: base + "shifts/1",
-            type: "GET",
-            dataType: "json",
-            headers: {
-                "Authorization": key,
-            },
-            success: function(data) {
-                for (var i = 0; i < data.length; i++) {
-                    var result = data[i];
-
-                    addShift(result.date, result.startTime, result.endTime, result.isSunday);
-                }
-            },
-            error: function(error) {
-                console.log("error: " + error);
-            }
-        });
+        getShifts(1);
     });
 
 });
